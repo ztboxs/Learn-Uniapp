c24f9094559a73c42da640720506410350e9b8d7
zt <960686470@qq.com>
2023-12-15 00:13:00 +0800

    feat: 添加前端基础模板和轮播图

---
+++ src/pages/index/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+//自定义导航栏
+import { getHomeBannerAPI } from '@/services/home'
+import type { BannerItem } from '@/types/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import CustomNavbar from './components/CustomNavbar.vue'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 页面加载
+onLoad(async () => {
+  getHomeBannerData()
+})
+</script>
+
+<template>
+  <!-- 自定义导航栏 -->
+  <CustomNavbar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper :list="bannerList" />
+  <text>index</text>
+</template>
+
+<style lang="scss">
+//
+</style>
---
+++ src/types/component.d.ts
@@ -0,0 +1,17 @@
+/**
+ * declare module '@vue/runtime-core'
+ *   现调整为
+ * declare module 'vue'
+ */
+import XtxSwiper from './XtxSwiper.vue'
+declare module 'vue' {
+  export interface GlobalComponents {
+    XtxSwiper: typeof XtxSwiper
+  }
+}
+import 'vue'
+declare module 'vue' {
+  export interface GlobalComponents {
+    //
+  }
+}
